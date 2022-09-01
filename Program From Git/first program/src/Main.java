241824aa00879a33908f35defd924fc5e6047880
fausberg <112504333+fausberg@users.noreply.github.com>
2022-09-01 16:26:40 +0300

    Add files via upload

---
+++ Program From Git/first program/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        First_Program();
+
+    }
+    public static void First_Program(){
+        for(int i = 0; i < 5; i++) {
+            System.out.println("My first program");
+        }
+    }
+
+}
